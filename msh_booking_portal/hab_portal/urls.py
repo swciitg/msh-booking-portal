fdf134b6a895ddba7af5a1e079ef31e58357c4b8
shrihaxori <ashrihariraju03@gmail.com>
2021-07-25 13:06:35 +0530

    modified url

--- msh_booking_portal/hab_portal/urls.py
+++ msh_booking_portal/hab_portal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HABView, name='all-view'),
+    path('view/', views.HABView, name='all-view'),
     #path('add/', views.HABCreate),
     #path('edit/', views.HABEdit),
     #path('<roll_number>/', views.StudentDetail.as_view(), name='stud-detail'),
