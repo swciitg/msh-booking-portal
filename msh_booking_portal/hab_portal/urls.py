708e8f9d38edccebe4202bd88029d6ddf850b41a
shrihaxori <ashrihariraju03@gmail.com>
2021-07-25 13:39:37 +0530

    hostel sorting left, basic view done

--- msh_booking_portal/hab_portal/urls.py
+++ msh_booking_portal/hab_portal/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('view/', views.HABView, name='all-view'),
-    #path('add/', views.HABCreate),
+    path('add/', views.HABCreate),
     #path('edit/', views.HABEdit),
     #path('<roll_number>/', views.StudentDetail.as_view(), name='stud-detail'),
     #path('thanks/', views.HABThanks),
