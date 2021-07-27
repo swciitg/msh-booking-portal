b59268918fe6f45932934f5d32acd92383855ad8
shrihaxori <ashrihariraju03@gmail.com>
2021-07-27 15:38:02 +0530

    added demo hostel caretaker access to each hostel

--- msh_booking_portal/hab_portal/views.py
+++ msh_booking_portal/hab_portal/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
 from .forms import HABForm
 from .models import HABModel
@@ -74,78 +75,91 @@ def HABDetail(request, slug):
     HABform = HABModel.objects.get(slug=slug)
     return render(request, 'hab_portal/hab-detail.html', { 'HABform': HABform })                   
 
+@permission_required('hab_portal.can_view_lohit_hostel_data')
 def LohitView(request):
     return render(request,
                   'hab_portal/lohit.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_brahma_hostel_data')
 def BrahmaView(request):
     return render(request,
                   'hab_portal/brahmaputra.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_siang_hostel_data')
 def SiangView(request):
     return render(request,
                   'hab_portal/siang.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_manas_hostel_data')
 def ManasView(request):
     return render(request,
                   'hab_portal/manas.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_disang_hostel_data')
 def DisangView(request):
     return render(request,
                   'hab_portal/disang.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_kameng_hostel_data')
 def KamengView(request):
     return render(request,
                   'hab_portal/kameng.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_umiam_hostel_data')
 def UmiamView(request):
     return render(request,
                   'hab_portal/umiam.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_barak_hostel_data')
 def BarakView(request):
     return render(request,
                   'hab_portal/barak.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_kapili_hostel_data')
 def KapiliView(request):
     return render(request,
                   'hab_portal/kapili.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_dihing_hostel_data')
 def DihingView(request):
     return render(request,
                   'hab_portal/dihing.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_suban_hostel_data')
 def SubanView(request):
     return render(request,
                   'hab_portal/subansiri.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_dhan_hostel_data')
 def DhanView(request):
     return render(request,
                   'hab_portal/dhansiri.html',
                   {'applications': HABModel.objects.all(),
                    'hostels': HABModel.hostel, })
 
+@permission_required('hab_portal.can_view_dhan_hostel_data')
 def MSHView(request):
     return render(request,
                   'hab_portal/msh.html',
