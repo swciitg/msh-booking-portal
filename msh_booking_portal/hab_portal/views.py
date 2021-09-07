0e19ca908c87669d7fbda06a38b0e5561318cecf
dhruvsomani <dsforweb@gmail.com>
2021-09-07 14:44:59 +0530

    Changed the HAB Model.

--- msh_booking_portal/hab_portal/views.py
+++ msh_booking_portal/hab_portal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
-# from .forms import HABForm
+from .forms import HABForm
 
 from .models import HABModel, HAB_FIELDS
 
