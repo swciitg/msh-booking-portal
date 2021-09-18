714871ac2469db79f45688895f7c7fe74f3551d6
dhruvsomani <dsforweb@gmail.com>
2021-09-18 11:49:37 +0530

    Added Model search functionality.

--- msh_booking_portal/hab_portal/admin.py
+++ msh_booking_portal/hab_portal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import HABModel
 
-
+class HABModelAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'user__user__username', 'roll_number', 'hostel']
 
 # Register your models here.
-admin.site.register(HABModel)
+admin.site.register(HABModel, HABModelAdmin)
